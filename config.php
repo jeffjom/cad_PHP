81b162972530978a412cc2018889ded577814a65
Jefferson Onofre Matavello <61469673+jeffjom@users.noreply.github.com>
2024-10-02 09:34:03 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
     // config.php
-
+    /*
     // Database connection settings
     $db_host = 'localhost';
     $db_username = 'root';
@@ -13,4 +13,18 @@
 
     // Set the PDO error mode to exceptions
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    */
+
+    try {
+        $db_host = 'localhost';
+        $db_username = 'root';
+        $db_password = '';
+        $db_name = 'cadastros';
+
+        // Create a PDO connection to the database
+        $dsn = "mysql:host=$db_host;dbname=$db_name";
+        $conn = new PDO($dsn, $db_username, $db_password);
+    } catch (PDOexception $e) {
+        echo 'Erro: ' . $e->getCode() . ' mensagem ' . $e->getMessage();
+    }
 ?>
